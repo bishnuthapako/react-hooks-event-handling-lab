748bae858553d44b522c09625e77b5a01f052f0c
Bishnu <bishnuthapako@gmail.com>
2022-06-05 03:05:40 -0400

    Completed

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,24 @@
 // Code EyesOnMe Component Here
+import react from 'react';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// function EyesOnMe(){
+   
+//     return <button onFocus={()=>{console.log("Good!")} }>Eyes on me</button>
+
+// }
+
+
+
+class EyesOnMe extends react.Component{
+    onBlur = ()=>{console.log("Hey! Eyes on me!")}
+    onFocus = ()=>{console.log('Good!')}
+    
+
+    render(){
+        return (<button onFocus={this.onFocus} onBlur={this.onBlur}>Eyes on me</button>)
+    }
+}
+
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,17 @@
 // Code Keypad Component Here
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function Keypad(){
+    
+    return (
+        <div>
+            <input type="password" onChange={()=>{console.log("Entering password...")}} />
+        </div>
+    )
+    
+}
+
+
+export default Keypad;
